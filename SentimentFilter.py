fd3da280187a9fa151445fbc6f233813a7ec2866
Diego Daolio <diegodaolio01@gmail.com>
2023-07-02 19:50:34 +0200

    prova

--- SentimentFilter.py
+++ SentimentFilter.py
@@ -5,9 +5,9 @@ class SentimentFilter(NumericRange):
     sentiment_filter = None
 
     def __init__(self, mode):
-        if mode >= 0:  # positive review filter
+        if mode > 0:  # positive review filter
             super().__init__("sentiment", 0, None, startexcl=True)
-        elif mode <= -0:  # negative review filter
+        elif mode < -0:  # negative review filter
             super().__init__("sentiment", None, 0, endexcl=True)
         else:  # no filter
             super().__init__("sentiment", None, None)
