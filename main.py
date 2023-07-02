6a47bf420f8f66e710b52f2b99f4cec2adc8404d
unknown <diegodaolio01@gmail.com>
2023-07-02 11:41:50 +0200

    Second commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from whoosh.index import create_in, open_dir
+from whoosh.fields import *
+import os, os.path
+
+if __name__ == '__main__':
+    print("Search Restaurant Reviews dsfsdf")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
