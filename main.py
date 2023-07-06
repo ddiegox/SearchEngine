76a655a5421677dfb1a5e96bd5b937a6c6a0d4ec
Diego Daolio <diegodaolio01@gmail.com>
2023-07-06 09:59:00 +0200

    json output

--- main.py
+++ main.py
@@ -16,17 +16,23 @@ import os, os.path
 import csv
 import argparse
 from math import log
+import json
 
-def run_query(f, query, filter, ranking, sentiment_filter=None):
-    f.write("\nQuery to analize: "+query)
+dcg_output = {}
+
+def run_query(f, text_query, filter, ranking, sentiment, sentiment_filter=None):
+    f.write("\nQuery to analize: "+text_query)
     ix = open_dir("indexdir")
+
+    if text_query not in dcg_output:
+        dcg_output[text_query] = []
 
     # Crea un oggetto di scoring
     scoringObject = SentimentWeighting(ranking)
 
     searcher = ix.searcher(weighting=scoringObject)
     parser = QueryParser("content", ix.schema)
-    query = parser.parse(query)
+    query = parser.parse(text_query)
 
     if filter == "1":
         query = And([query, sentiment_filter])
@@ -45,6 +51,8 @@ def run_query(f, query, filter, ranking, sentiment_filter=None):
     f.write("DCG = " + str(dcg)+"\n")
     f.write("-------------------------\n")
 
+    dcg_output[text_query].append({"sentiment": "VADER" if sentiment==1 else "TextBlob", "ranking": "TF-IDF" if ranking==1 else "BM25F", "dcg": dcg})
+
 def run(f, documents_list, schema, ranking=1, sentiment=1, test_query=None):
 
     ix = create_in("indexdir", schema)
@@ -52,7 +60,7 @@ def run(f, documents_list, schema, ranking=1, sentiment=1, test_query=None):
     # create writer
     writer = ix.writer()
 
-    # create sentiment intensity analyzer (Vader)
+    # create sentiment intensity analyzer (VADER)
     sentiment_analyzer = SentimentIntensityAnalyzer()
 
     f.write("RUN\n\n")
@@ -61,7 +69,7 @@ def run(f, documents_list, schema, ranking=1, sentiment=1, test_query=None):
     if sentiment == 2:
         f.write("TextBlob\n")
     else:
-        f.write("Vader\n")
+        f.write("VADER\n")
     f.write("\nIR Model used: "+"\n")
     if(ranking == 2):
         f.write("BM25F\n")
@@ -87,23 +95,24 @@ def run(f, documents_list, schema, ranking=1, sentiment=1, test_query=None):
 
     if test_query is None:
         query = input("Insert a query: ")
+        query = query.strip()
         filter = input("Insert 1 for filter, any value for not filter: ")
         sentiment_filter = None
         if filter == "1":
             filter_value = int(input("Insert -1 for negative reviews or 1 for positive reviews: "))
             sentiment_filter = SentimentFilter(filter_value)
 
-        run_query(f, query, filter, ranking,sentiment_filter)
+        run_query(f, query, filter, ranking,sentiment,sentiment_filter)
     else:
         for query in test_query:
-            run_query(f, query, "0", ranking)
+            run_query(f, query, "0", ranking, sentiment)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Search Restaurant Reviews")
     parser.add_argument('-t', '--test', default=0, type=int, help="Se presente, indica che lo script deve essere attivato in modalità test")
-    parser.add_argument('-s', '--sentiment', default=1, type=int, help="Indica la libreria di Sentiment Analysis da utilizzare (1-Vader, 2-Textblob), default: Vader")
-    parser.add_argument('-r', '--ranking', default=1, type=int, help="Indica la funzione di ranking da utilizzare (1-TF-IDF (modello vettoriale), 2-BM25 (modello probabilistico)), default: TF-IDF")
+    parser.add_argument('-s', '--sentiment', default=1, type=int, help="Indica la libreria di Sentiment Analysis da utilizzare (1-VADER, 2-Textblob), default: VADER")
+    parser.add_argument('-r', '--ranking', default=1, type=int, help="Indica la funzione di ranking da utilizzare in combinazione con la sentiment analysis (1-TF-IDF (modello vettoriale), 2-BM25 (modello probabilistico)), default: TF-IDF")
     args = parser.parse_args()
 
     sentiment = int(args.sentiment)
@@ -111,7 +120,7 @@ if __name__ == '__main__':
     test = int(args.test)
 
     nltk.download('wordnet')
-    nltk.download('vader_lexicon')
+    nltk.download('VADER_lexicon')
 
     # use csv parser to read reviews
     documents_list = []
@@ -126,7 +135,7 @@ if __name__ == '__main__':
         test_query = []
         with open('src/benchmark_queries', 'r') as f:
             for line in f:
-                test_query.append(line)
+                test_query.append(line.strip())
 
     # create analyzer for preprocessing
     analyzer = RegexTokenizer() | LowercaseFilter() | StopFilter() | LemmaFilter() | StemFilter()
@@ -146,6 +155,7 @@ if __name__ == '__main__':
         os.mkdir("indexdir")
 
     output_file = 'output_file.txt'
+    output_json = 'dcg_output.json'
 
     with open(output_file, 'w') as f:
         f.write("Search Restaurant Reviews\n\n")
@@ -157,3 +167,7 @@ if __name__ == '__main__':
             run(f, documents_list, schema, 2, 2, test_query)
         else:
             run(f, documents_list, schema, ranking, sentiment, test_query)
+
+        with open(output_json, 'w') as f2:
+            print(dcg_output)
+            f2.write(json.dumps(dcg_output, indent=4))
